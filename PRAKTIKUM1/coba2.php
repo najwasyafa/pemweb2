60d8e89d5542ffc3d375dc7cdcdd08cca848f213
najwasyafa <114551395+najwasyafa@users.noreply.github.com>
2023-03-02 10:15:16 +0700

    Add files via upload

---
+++ PRAKTIKUM1/coba2.php
@@ -0,0 +1,7 @@
+<?php
+
+    $tims = ["Najwa", "Syafa", "Kamila", "Nasya"];
+
+    foreach($tims as $person){
+        echo $person. '<br>' ;
+    }
---
+++ PRAKTIKUM1/index.php
@@ -0,0 +1,9 @@
+<?php
+    $nama = 'Najwa Syafa Kamila';
+    $umur = 18;
+    $berat = 45;
+
+    echo 'Nama saya : ' . $nama;
+    echo '<br> Umur saya : ' . $umur;
+    echo '<br> Berat saya : ' . $berat;
+?>
